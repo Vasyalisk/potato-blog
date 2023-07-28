9264954fc76a5263cb726aeb686f3b13ae67522b
Vasylchenko <vasylchenko@pixelfield.cz>
2023-07-28 18:16:37 +0200

    Fixed script

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -4,4 +4,4 @@ python manage.py on_run_check
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py create_default_admin
-gunicorn wsgi:application --bind "${HOST:-0.0.0.0}:${PORT:-8000}
+gunicorn wsgi:application --bind "${HOST:-0.0.0.0}:${PORT:-8000}"
